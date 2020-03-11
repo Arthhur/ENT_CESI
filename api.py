b6012ab0bdce78598af437edd0d5c67b73693165
arthhur <arthur.prodhomme@yahoo.fr>
2020-03-11 15:59:30 +0100

    fix(api)

--- api.py
+++ api.py
@@ -1,24 +1,46 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 from flask_cors import CORS, cross_origin
 from flask_restful import reqparse, abort, Api, Resource
+from flask import request
+
 from models import Person
 from database import Database
 
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app)
+parser = reqparse.RequestParser()
 
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
+# ElevesList
+class Eleve(Resource):
+    def __init__(self):
+        self.db = Database()
+
+    def delete(self, eleve_id):
+        self.db.delete(eleve_id)
+
+    def put(self, eleve_id):
+        data = request.get_json()
+        self.db.update(eleve_id, data)
+
+
 class ElevesList(Resource):
-    def get(self):
-        db = Database()
-        return jsonify(db.list_eleves())
+    def __init__(self):
+        self.db = Database()
 
+    def get(self):
+        return jsonify(self.db.list_eleves())
+
+    def post(self):
+        data = request.get_json()
+        self.db.insert(data)
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(ElevesList, '/eleves')
+api.add_resource(Eleve, '/eleves/<eleve_id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -8,8 +8,50 @@ class Database:
         db = "ent_cesi"
         self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
                                    DictCursor)
-        self.cur = self.con.cursor()
+        self.cursor = self.con.cursor()
     def list_eleves(self):
-        self.cur.execute("SELECT id, nom FROM eleve")
-        result = self.cur.fetchall()
+        self.cursor.execute("SELECT id, "
+                            "nom, "
+                            "prenom, "
+                            "url_image, "
+                            "date_naissance, "
+                            "date_debut ,"
+                            "date_fin,"
+                            "nom_promotion"
+                            "FROM eleve"
+                            "INNER JOIN promotion ON promotion.id = eleve.id_promotion")
+        result = self.cursor.fetchall()
         return result
+
+    def eleve_by_id(self, eleve_id):
+        sql = "SELECT id, nom, prenom, url_image, date_naissance, date_debut , date_fin, nompromotion FROM eleve INNER JOIN promotion ON promotion.id = eleve.id_promotion WHERE eleve.id = %s"
+        self.cursor.execute(sql, eleve_id)
+        result = self.cursor.fetchall()
+        return result
+
+    def delete(self, eleve_id):
+        sql = "DELETE FROM eleve WHERE id = %s"
+        self.cursor.execute(sql, eleve_id)
+        self.con.commit()
+        print(self.cursor.rowcount, " record(s) deleted")
+
+    def insert(self, eleve):
+        nom = eleve.get('nom')
+        prenom = eleve.get('prenom')
+        url_image = eleve.get('url_image')
+        date_naissance = eleve.get('date_naissance')
+        id_promotion = eleve.get('id_promotion')
+        sql = "INSERT INTO eleve (nom, prenom, url_image, date_naissance, id_promotion) VALUES (%s, %s, %s, %s, %s)"
+        data = (nom, prenom, url_image, date_naissance, id_promotion)
+        self.cursor.execute(sql, data)
+        self.con.commit()
+
+    def update(self, eleve_id, eleve):
+        nom = eleve.get('nom')
+        prenom = eleve.get('prenom')
+        url_image = eleve.get('url_image')
+        date_naissance = eleve.get('date_naissance')
+        id_promotion = eleve.get('id_promotion')
+        sql = "UPDATE eleve SET nom = %s, prenom = %s, url_image = %s, date_naissance = %s, id_promotion = %s WHERE eleve.id = %s"
+        data = (nom, prenom, url_image, date_naissance, id_promotion, eleve_id)
+        self.cursor.execute(sql, data)
